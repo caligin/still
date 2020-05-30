2948d268010b7e14f4a0361dfdb02973906d723a
0xf00 <foo@anima.tech>
2020-05-30 01:08:56 +0100

    enh: sketching

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite().mount("/", routes![index]).launch();
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fs::File;
+    use std::io::BufReader;
+    use std::io::prelude::*;
+
+    #[test]
+    fn sketch() {
+        let file = File::open("data.sample.log").expect("failed to file");
+        let buf_reader = BufReader::new(file);
+        let filtered = buf_reader.lines()
+            .map(|l| l.unwrap())
+            .filter(|line| line.contains("ingress"));
+        let result: String = filtered.collect();
+        
+        println!("AAAAAAAAAAAAAAAAAAS {}", result);
+    }
 }
