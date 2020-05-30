6ef6e73e334c972927b8e4bc7d823f5448983b07
0xf00 <foo@anima.tech>
2020-05-30 11:20:23 +0100

    enh: more sketching

--- src/main.rs
+++ src/main.rs
@@ -19,13 +19,26 @@ mod tests {
 
     #[test]
     fn sketch() {
+
+        // ingress protocol.kitchen !feedme !"GET /assets"
+        // | where stream != "stderr"
+        // | where kubernetes.namespace_name = "protocol-kitchen"
+        // | parse log with /"([^ ]+) ([^ ]+) HTTP\/1.1" ([\d]{3})/ as verb, path, response_code
+        // | where response_code = 200
+        // | count by verb, path # multi-count maybe not mvp
+        // | sort by _count
+
         let file = File::open("data.sample.log").expect("failed to file");
         let buf_reader = BufReader::new(file);
         let filtered = buf_reader.lines()
             .map(|l| l.unwrap())
-            .filter(|line| line.contains("ingress"));
+            .filter(|line| line.contains("protocol.kitchen"))
+            .filter(|line| !line.contains("feedme"))
+            .filter(|line| !line.contains("GET /assets"));
+            
+            // .map(f: F);
         let result: String = filtered.collect();
         
-        println!("AAAAAAAAAAAAAAAAAAS {}", result);
+        println!("{}", result);
     }
 }
