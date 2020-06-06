67f510cef8d2f21112437d7aed9911a394ce88af
0xf00 <foo@anima.tech>
2020-06-06 18:52:04 +0100

    enh: better understand types

--- src/main.rs
+++ src/main.rs
@@ -233,5 +233,65 @@ mod tests {
         assert_eq!(1 ,test_visitor.sort_fields);
     }
 
+    #[test]
+    fn sketch_type_breakdown() {
+        let group_keys = vec!["verb", "path"];
+
+        let file = File::open("mini.sample.log").expect("failed to file");
+        let buf_reader = BufReader::new(file);
+
+        let filtered1: &mut dyn Iterator<Item = std::result::Result<std::string::String, std::io::Error>> = &mut buf_reader.lines();
+        let filtered2: &mut dyn Iterator<Item = String> = &mut filtered1.map(|l| l.unwrap());
+        // let filtered3: &mut dyn Iterator<Item = String> = &mut filtered2.filter(|line| line.contains("ingress")); LOL THIS FILTERS TOO MUCH, MISSED IN ORIGINAL TEST!
+        let filtered4: &mut dyn Iterator<Item = String> = &mut filtered2.filter(|line| line.contains("protocol.kitchen"));
+        let filtered5: &mut dyn Iterator<Item = String> = &mut filtered4.filter(|line| !line.contains("feedme"));
+        let filtered6: &mut dyn Iterator<Item = String> = &mut filtered5.filter(|line| !line.contains("GET /assets"));
+        let filtered7: &mut dyn Iterator<Item = Value> = &mut filtered6.filter_map(|line| serde_json::from_str(&line).ok());
+        let filtered8: &mut dyn Iterator<Item = Value> = &mut filtered7.filter(|json: &Value| json["stream"] != "stderr");
+        let filtered9: &mut dyn Iterator<Item = Value> = &mut filtered8.filter(|json| json["kubernetes"]["namespace_name"] == "protocol-kitchen");
+        let filtered10: &mut dyn Iterator<Item = Value> = &mut filtered9.map(|mut json: Value| {
+            let re = Regex::new(r#""([^ ]+) ([^ ]+) HTTP/1.1" ([\d]{3})"#).unwrap();
+            let cap = re.captures(json["log"].as_str().unwrap()).unwrap(); // actually here we want to filter out lines that don't match tbh
+            let aliases = vec!["verb", "path", "response_code"];
+            let aliases_and_value: Vec<(&&str, String)> = aliases
+                .iter().enumerate()
+                .map(|(idx, alias)| (alias, cap[idx + 1].to_owned()))
+                .collect();
+            for (alias, value) in aliases_and_value {
+                json[alias] = Value::String(value);
+            }
+            json
+        });
+        let filtered11: &mut dyn Iterator<Item = Value> = &mut filtered10.filter(|json| json["response_code"] == "200");
+        let filtered12: HashMap<Vec<String>, usize> = filtered11.fold(HashMap::new(), |mut acc, json| {
+            let identity: Vec<String> = group_keys.iter().map(|k| json[k].as_str().unwrap().to_owned()).collect();
+            let counter = acc.entry(identity).or_insert(0);
+            *counter += 1;
+            acc
+        });
+        let filtered13: &mut dyn Iterator<Item = Value> = &mut filtered12.iter().map(|(k, v)| {
+            let mut json = json!({});
+            let group_keys = vec!["verb", "path"];
+            for (i, key) in group_keys.iter().enumerate() {
+                json[key] = json!(k[i]);
+            }
+            json["_count"] = json!(v);
+            json
+        });
+        let mut filtered = filtered13.collect::<Vec<Value>>();
+        
+        filtered.sort_by(|a, b| serde_json::to_string(&b["_count"]).unwrap().cmp(&serde_json::to_string(&a["_count"]).unwrap()));
+
+        let got = filtered;
+        let expected = vec![
+            json!({"_count":4,"path":"/index.html","verb":"GET"}),
+            json!({"_count":3,"path":"/","verb":"GET"}),
+            json!({"_count":2,"path":"/a-tale-of-two-clams/","verb":"GET"}),
+            json!({"_count":1,"path":"/lazy-loaf-tin-bakes/","verb":"GET"})];
+
+        assert_eq!(expected, got);
+    }
+
+
 }
 
